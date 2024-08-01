2f6cc9db20421b0a182f20013560669f90afb318
noblet <appiahpatricknoblet@gmail.com>
2024-08-01 23:53:21 +0100

    Added a solution to the lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a utility function for determining if all
+boxes can be unlocked.
+
+It includes the `canUnlockAll` function, which checks whether it is
+possible to unlock all boxes given a list of boxes, where each box
+contains a list of keys to other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked starting from the first box.
+
+    This function checks if it is possible to unlock all boxes given a
+    list of boxes, where each box contains keys to other boxes.
+    It starts with the first box (index 0) and attempts to unlock all
+    other boxes using the keys found in each box.
+
+    Parameters:
+    boxes (List[List[int]]): A list of lists where each inner list
+    contains integers representing keys to other boxes.
+
+    Returns:
+    bool: True if all boxes can be unlocked, False otherwise.
+
+    Example:
+    >>> boxes = [[1, 2], [2, 3], [3], []]
+    >>> canUnlockAll(boxes)
+    True
+
+    >>> boxes = [[1], [2], [3], []]
+    >>> canUnlockAll(boxes)
+    False
+    """
+    num_of_boxes = len(boxes)
+    visited = [False] * num_of_boxes
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        if not visited[box]:
+            visited[box] = True
+
+            new_boxes = [key for key in boxes[box] if not visited[key]]
+            queue.extend(new_boxes)
+
+    return all(visited)
